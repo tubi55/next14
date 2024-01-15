e698e472148c08765ec47bbff1da3306f6800f6c
tubi55 <tubi55@nate.com>
2024-01-15 15:48:32 +0900

    page컴포넌트에서 에러 발생시 출력할 페이지 커스터마이징

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,10 @@ import clsx from 'clsx';
 import styles from './about.module.scss';
 
 export default function About() {
-  return (
-    <section className={clsx(styles.about)}>
-      <h1>About</h1>
-    </section>
-  );
+	throw new Error('Error in About page..');
+	return (
+		<section className={clsx(styles.about)}>
+			<h1>About</h1>
+		</section>
+	);
 }
