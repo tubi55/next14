2af84c00026bd99a6a3db2c431841fdd5a64fdab
tubi55 <tubi55@nate.com>
2024-01-18 09:45:41 +0900

    글작성, 글수정시 추천 이미지 패널에서 이미지URL 등록

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -1,8 +1,7 @@
 import clsx from 'clsx';
 import styles from './write.module.scss';
 import { addPost } from '@/lib/actions';
-import Unsplash from '@/components/unsplash/Unsplash';
-import BtnUnsplash from '@/components/btnUnsplash/BtnUnsplash';
+import InputImage from '@/components/inputImage/InputImage';
 
 export default function Write() {
 	return (
@@ -12,9 +11,8 @@ export default function Write() {
 
 				<form action={addPost}>
 					<input type='text' placeholder='title' name='title' />
-					<input type='text' placeholder='image URL' name='img' />
+					<InputImage />
 					<textarea name='desc' cols='30' rows='3' placeholder='description'></textarea>
-					<BtnUnsplash />
 
 					<nav>
 						<input type='reset' value='cancel' />
@@ -22,7 +20,6 @@ export default function Write() {
 					</nav>
 				</form>
 			</section>
-			<Unsplash />
 		</>
 	);
 }
--- src/hooks/useGlobalData.js
+++ src/hooks/useGlobalData.js
@@ -5,8 +5,11 @@ export const GlobalContext = createContext();
 export function GlobalProvider({ children }) {
 	const [MenuOpen, setMenuOpen] = useState(false);
 	const [ImgPanelOpen, setImgPanelOpen] = useState(false);
+	const [ImgUrl, setImgUrl] = useState('');
 
-	return <GlobalContext.Provider value={{ MenuOpen, setMenuOpen, ImgPanelOpen, setImgPanelOpen }}>{children}</GlobalContext.Provider>;
+	return (
+		<GlobalContext.Provider value={{ MenuOpen, setMenuOpen, ImgPanelOpen, setImgPanelOpen, ImgUrl, setImgUrl }}>{children}</GlobalContext.Provider>
+	);
 }
 
 export function useGlobalData() {
