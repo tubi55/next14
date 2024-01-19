19e7df720543c988c8f77739752746fd81fc4f18
tubi55 <tubi55@nate.com>
2024-01-19 10:56:13 +0900

    Login form basic styling

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,10 +12,10 @@ const checkUserDB = async credentials => {
 		connectDB();
 
 		const user = await User.findOne({ username: credentials.username });
-		if (!user) throw new Error('Wrong credentials!');
+		if (!user) throw new Error('there is no username yout input in DB!!');
 
 		const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-		if (!isPasswordCorrect) throw new Error('Wrong credentials!');
+		if (!isPasswordCorrect) throw new Error('now matched password from DB!');
 
 		return user;
 	} catch (err) {
