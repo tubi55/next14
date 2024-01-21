261a27ef99a362d3d681811e6a758a24023bf623
tubi55 <tubi55@nate.com>
2024-01-21 14:40:49 +0900

    이미지 업로드 완료

---
+++ src/components/imageUploader/ImageUploader.jsx
@@ -0,0 +1,19 @@
+'use client';
+import { UploadButton } from '@/utils/uploadthing';
+
+export default function ImageUploader() {
+	return (
+		<div>
+			<UploadButton
+				endpoint='imageUploader'
+				onClientUploadComplete={res => {
+					console.log('Files: ', res);
+					alert('Upload Completed');
+				}}
+				onUploadError={error => {
+					alert(`ERROR! ${error.message}`);
+				}}
+			/>
+		</div>
+	);
+}
