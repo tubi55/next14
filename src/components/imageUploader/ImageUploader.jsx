b4dad7c46c416d275fc798a5fb796ab2f68dea04
tubi55 <tubi55@nate.com>
2024-01-21 15:05:32 +0900

    업로드된 이미지 url db에 저장하여 출력

--- src/components/imageUploader/ImageUploader.jsx
+++ src/components/imageUploader/ImageUploader.jsx
@@ -1,13 +1,17 @@
 'use client';
+import { useGlobalData } from '@/hooks/useGlobalData';
 import { UploadButton } from '@/utils/uploadthing';
 
 export default function ImageUploader() {
+	const { setImgUrl } = useGlobalData();
 	return (
 		<div>
 			<UploadButton
 				endpoint='imageUploader'
 				onClientUploadComplete={res => {
 					console.log('Files: ', res);
+					console.log(res[0].url);
+					setImgUrl(res[0].url);
 					alert('Upload Completed');
 				}}
 				onUploadError={error => {
